f46fcb4a19db902dd5f2a143d6939004c1fbbcd7
Saumya <sg5290@nyu.edu>
2018-12-15 00:29:05 -0500

    Modified main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, utils
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 # %matplotlib inline
@@ -144,8 +144,8 @@ def train(epoch):
                 100. * batch_idx / len(train_loader), loss.data[0]))
     return correct
 
-
-
+# newimage_array=torch.empty()
+newimage_array=[]
 def validation():
     model.eval()
     validation_loss = 0
@@ -154,12 +154,13 @@ def validation():
         data, target = (Variable(data, volatile=True)), (Variable(target))
         data_lab = colors.rgb_to_lab(data)
         data_l = data_lab[:,0,:,:].unsqueeze(1)
+        output_l=data_lab[:,0,:,:]
         # print("L",end="")
         # print(data_l.size())
         data_ab = data_lab[:,1:,:,:]
         # data_lab = colors.rgb_to_lab(data)
         # data_l = data_lab[0]
-        target = data_ab
+        target = data_ab.float()
         output = model(data_l)
         # data_l = data_lab[0]
         # target = data_lab
@@ -167,9 +168,20 @@ def validation():
         # output = model(data)
         # validation_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
         validation_loss += criterion(output, target).data[0]
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).sum()
-
+        # pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        # correct += pred.eq(target.data.view_as(pred)).sum()
+        #Compare original LAB vs data_l+output(for ab)
+        output_a=output[:,0,:,:].detach()
+        output_b=output[:,1,:,:].detach()
+        print(output_l.size())
+        print(output_a.size())
+        print(output_b.size())
+        final_output=torch.stack([output_l,output_a,output_b],dim=1)
+        print(final_output.size())
+        # new = final_output[0].permute(1, 2, 0)
+        # plt.imshow(new)
+        # plt.show()
+        newimage_array.append(final_output[0])
     validation_loss /= len(val_loader.dataset)
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         validation_loss, correct, len(val_loader.dataset),
@@ -186,10 +198,24 @@ for epoch in range(1, args.epochs + 1):
     torch.save(model.state_dict(), model_file)
     print('\nSaved model to ' + model_file + '. You can run `python evaluate.py --model' + model_file + '` to generate the Kaggle formatted csv file')
 
-plt.plot(tr_acc)
-plt.title("Training accuracy")
-plt.show()
+i=0
+for new_image in newimage_array:
+    i+=1
+    print(new_image.size())
+    print(new_image.type())
+    rgb_pred_image = colors.lab_to_rgb(new_image.unsqueeze(0))
+    print(rgb_pred_image.size())
+    # new = rgb_pred_image[0].permute(1, 2, 0)
+    # plt.imshow(new)
+    # plt.show()
+    s=args.data+"/img/iimages"+str(i)+".jpeg"
+    utils.save_image(rgb_pred_image[0],s)
 
-plt.plot(val_acc)
-plt.title("Validation accuracy")
-plt.show()
+
+# plt.plot(tr_acc)
+# plt.title("Training accuracy")
+# plt.show()
+#
+# plt.plot(val_acc)
+# plt.title("Validation accuracy")
+# plt.show()
